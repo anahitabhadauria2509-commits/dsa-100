33462615e6717c2d06282bcb79a7990c20944b13
Anahita <anahitabhadauria2509@gmail.com>
2026-04-26 12:00:00 +0530

    Day(85).c

--- Day(85).c
+++ Day(85).c
@@ -1,61 +1,32 @@
 #include <stdio.h>
 
-#define MAX 100
-
-void merge(int arr[], int left, int mid, int right) {
-    int temp[MAX];
-    int i = left, j = mid + 1, k = left;
-
-    // Merge two sorted halves
-    while (i <= mid && j <= right) {
-        if (arr[i] <= arr[j])
-            temp[k++] = arr[i++];
-        else
-            temp[k++] = arr[j++];
-    }
-
-    // Copy remaining elements
-    while (i <= mid)
-        temp[k++] = arr[i++];
-
-    while (j <= right)
-        temp[k++] = arr[j++];
-
-    // Copy back to original array
-    for (int p = left; p <= right; p++)
-        arr[p] = temp[p];
+void merge(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1, n2 = r - m;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (int j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
 }
 
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = (left + right) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-
-    int arr[MAX];
-
-    // Input array
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    mergeSort(arr, 0, n - 1);
-
-    // Output sorted array
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
-    }
-
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    mergeSort(arr, 0, 6);
+    for (int i=0; i < 7; i++) printf("%d ", arr[i]);
     return 0;
 }
+
 
 
 
