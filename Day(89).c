5758595affb1d3d4ea548c52840368b43220a342
Anahita <anahitabhadauria2509@gmail.com>
2026-04-30 12:00:00 +0530

    Day(89).c

--- Day(89).c
+++ Day(89).c
@@ -1,60 +1,32 @@
 #include <stdio.h>
 
-// Function to check if allocation is possible
-int isPossible(int arr[], int n, int m, int maxPages) {
-    int students = 1;
-    int sum = 0;
-
+int isFeasible(int arr[], int n, int m, int cur_min) {
+    int students = 1, sum = 0;
     for (int i = 0; i < n; i++) {
-        // If a single book has more pages than maxPages
-        if (arr[i] > maxPages)
-            return 0;
-
-        if (sum + arr[i] > maxPages) {
+        if (arr[i] > cur_min) return 0;
+        if (sum + arr[i] > cur_min) {
             students++;
             sum = arr[i];
-
-            if (students > m)
-                return 0;
-        } else {
-            sum += arr[i];
-        }
+            if (students > m) return 0;
+        } else sum += arr[i];
     }
     return 1;
 }
 
 int main() {
-    int n, m;
-    scanf("%d %d", &n, &m);
-
-    int arr[n];
+    int arr[] = {12, 34, 67, 90}, n = 4, m = 2;
     int sum = 0, max = 0;
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
-        if (arr[i] > max)
-            max = arr[i];
+    for(int i=0; i<n; i++) { sum += arr[i]; if(arr[i]>max) max = arr[i]; }
+    int low = max, high = sum, ans = sum;
+    while(low <= high) {
+        int mid = low + (high-low)/2;
+        if(isFeasible(arr, n, m, mid)) { ans = mid; high = mid - 1; }
+        else low = mid + 1;
     }
-
-    int low = max;
-    int high = sum;
-    int ans = sum;
-
-    while (low <= high) {
-        int mid = (low + high) / 2;
-
-        if (isPossible(arr, n, m, mid)) {
-            ans = mid;
-            high = mid - 1;  // try smaller maximum
-        } else {
-            low = mid + 1;   // increase limit
-        }
-    }
-
-    printf("%d\n", ans);
+    printf("Min Pages: %d", ans);
     return 0;
 }
+
 
 
 
