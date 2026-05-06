854dec83d53c3f0ad29a4bc292f058a86a948bc7
Anahita <anahitabhadauria2509@gmail.com>
2026-05-06 12:00:00 +0530

    Day(95).c

--- Day(95).c
+++ Day(95).c
@@ -1,75 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Pair {
-    int value;
-    int index;
-};
+struct Interval { int s, e; };
+int compare(const void* a, const void* b) { return ((struct Interval*)a)->s - ((struct Interval*)b)->s; }
 
-void merge(struct Pair arr[], int left, int mid, int right, int count[]) {
-    int i = left, j = mid + 1, k = 0;
-    int tempSize = right - left + 1;
-    struct Pair temp[tempSize];
-
-    int rightCount = 0;
-
-    while (i <= mid && j <= right) {
-        if (arr[i].value <= arr[j].value) {
-            count[arr[i].index] += rightCount;
-            temp[k++] = arr[i++];
+void mergeIntervals(struct Interval arr[], int n) {
+    qsort(arr, n, sizeof(struct Interval), compare);
+    int index = 0;
+    for (int i = 1; i < n; i++) {
+        if (arr[index].e >= arr[i].s) {
+            if (arr[index].e < arr[i].e) arr[index].e = arr[i].e;
         } else {
-            rightCount++;
-            temp[k++] = arr[j++];
+            index++;
+            arr[index] = arr[i];
         }
     }
-
-    while (i <= mid) {
-        count[arr[i].index] += rightCount;
-        temp[k++] = arr[i++];
-    }
-
-    while (j <= right) {
-        temp[k++] = arr[j++];
-    }
-
-    for (i = left, k = 0; i <= right; i++, k++) {
-        arr[i] = temp[k];
-    }
-}
-
-void mergeSort(struct Pair arr[], int left, int right, int count[]) {
-    if (left < right) {
-        int mid = (left + right) / 2;
-
-        mergeSort(arr, left, mid, count);
-        mergeSort(arr, mid + 1, right, count);
-
-        merge(arr, left, mid, right, count);
-    }
+    for (int i = 0; i <= index; i++) printf("[%d, %d] ", arr[i].s, arr[i].e);
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-
-    struct Pair arr[n];
-    int count[n];
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i].value);
-        arr[i].index = i;
-        count[i] = 0;
-    }
-
-    mergeSort(arr, 0, n - 1, count);
-
-    // Print result
-    for (int i = 0; i < n; i++) {
-        printf("%d ", count[i]);
-    }
-
+    struct Interval arr[] = {{1,3}, {2,4}, {5,7}, {6,8}};
+    mergeIntervals(arr, 4);
     return 0;
 }
+
 
 
 
